858e8121153b15a721156a78b7a3da8b4b4959b9
Mariana Lima <81395468+marimagalhaesl@users.noreply.github.com>
2023-04-20 20:46:32 -0300

    Update Ex01AuraListHelper.js

--- Ex01AuraListHelper.js
+++ Ex01AuraListHelper.js
@@ -1,5 +1,17 @@
 ({
-	handleSucessHelper : function(component, event, helper) {
-		component.set("v.mensagem", "Lead criado com sucesso");
-	}
+    handleSucessHelper : function(component, event, helper) {
+        var toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({
+            "title": "Lead inserido!",
+            "message": "O lead foi inserido com sucesso.",
+            "type": "success"
+        });
+        toastEvent.fire();
+    },
+    
+    handleResetHelper: function(component, event, helper) {
+        component.find('field').forEach(function(f) {
+            f.reset();
+        });
+    }
 })
